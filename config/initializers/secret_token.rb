ad65e8c81f1c92b4e9ddd1b466a62cac9e1e12fb
jhylau <jhylau1@gmail.com>
2013-05-22 11:15:34 -0700

    first commit - added database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalblog::Application.config.secret_token = '09aeb900f0bdcf9da6331f6e9663ce63da8bbfa2accacdb3243a98c5beaa2a7cf4342232892f3de0427e270b1dfe6845edd22244cb69d893d719b8a3d5e7a42b'
